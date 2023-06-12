be3aa4378586ecdbd40f8da4a4a837aaa639bfd6
ryuki8643 <garyuki88me@gmail.com>
2023-06-13 02:42:18 +0900

    demo app

--- app.py
+++ app.py
@@ -1,37 +1,20 @@
 import streamlit as st
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy.io.wavfile import read
 import os
-import base64
 
-# Title
-st.title('Upload and Visualize WAV File')
 
-# Text input
+# title
+st.title('Play Local WAV File')
+
+# text input
 text = st.text_input('Please enter text')
 
-# File upload
-uploaded_file = st.file_uploader("Please upload a WAV file", type="wav")
-if uploaded_file is not None:
-    # Temporarily save the file
-    with open('temp.wav', 'wb') as f:
-        f.write(uploaded_file.getbuffer())
-    
-    # Read the file
-    rate, data = read('temp.wav')
-    
-    # Play the file (provide a link)
-    audio_file = open('temp.wav', 'rb')
-    audio_bytes = audio_file.read()
-    st.audio(audio_bytes, format='audio/wav')
-    
-    # Visualize the waveform
-    fig, ax = plt.subplots()
-    time = np.arange(0, len(data)) / rate
-    ax.plot(time, data)
-    ax.set(xlabel='Time [s]', ylabel='Amplitude')
-    st.pyplot(fig)
-    
-    # Delete the temporary file
-    os.remove('temp.wav')
+# file selection and playback
+wav_files = ['output.wav', 'input.wav']
+for file in wav_files:
+    if os.path.isfile(file):
+        if st.button(f'Play {file}'):
+            audio_file = open(file, 'rb')
+            audio_bytes = audio_file.read()
+            st.audio(audio_bytes, format='audio/wav')
+    else:
+        st.write(f'{file} does not exist.')
